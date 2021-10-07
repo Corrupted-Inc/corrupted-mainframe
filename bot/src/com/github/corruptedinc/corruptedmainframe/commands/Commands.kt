9e5a7a81d573f18f12dbb29f53e5677f3b962987
/v/ (The Corrupted King) <66529179+TheCorruptedKing@users.noreply.github.com>
2021-10-07 15:14:58 -0400

    Update Commands.kt

--- bot/src/com/github/corruptedinc/corruptedmainframe/commands/Commands.kt
+++ bot/src/com/github/corruptedinc/corruptedmainframe/commands/Commands.kt
@@ -158,7 +158,7 @@ class Commands(val bot: Bot) {
             .addOption(OptionType.STRING, "message", "Message link", true)
             .addOption(OptionType.STRING, "reactions", "Reactions", true))
             .addOption(OptionType.STRING, "role", "role name", true)) { event ->
-                    // case sensitive because of unpingable roles
+                    // case sensitive because of unpingable roles. Plan on adding role IDs as an option.
             assertAdmin(event)
 
             val reactionsMap = event.getOption("reactions")!!.asString.removeSurrounding("\"").split(", ")
