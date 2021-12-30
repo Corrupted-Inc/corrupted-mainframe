162f3ad3b642b3d9b42194e77e62474b99c5a3ee
Xenon <Remy.todd@@students.rsd.edu>
2021-12-29 20:30:34 -0800

    Log4J BEGONE!

--- bot/src/com/github/corruptedinc/corruptedmainframe/Main.kt
+++ bot/src/com/github/corruptedinc/corruptedmainframe/Main.kt
@@ -2,16 +2,9 @@ package com.github.corruptedinc.corruptedmainframe
 
 import com.beust.klaxon.JsonParsingException
 import com.github.corruptedinc.corruptedmainframe.discord.Bot
-import org.apache.log4j.BasicConfigurator
-import org.apache.log4j.Level
-import org.apache.log4j.Logger
 import java.io.File
 
 fun main() {
-    // Test
-    BasicConfigurator.configure()  // Fixes log4j warning
-    Logger.getRootLogger().level = Level.ERROR
-
     val config = Config.load(File("config.json"))
     config ?: throw JsonParsingException("Failed to load configuration!")
     Bot(config)
